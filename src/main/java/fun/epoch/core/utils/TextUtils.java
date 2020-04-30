c05ed66e2c68564b670350059b2e65e8bd6cb3c9
epochwz <epoch.wz@gmail.com>
2020-04-30 09:50:25 +0800

    feat<utils>: update TextUtils
    add method: isNotBlank & isBlank

--- src/main/java/fun/epoch/core/utils/TextUtils.java
+++ src/main/java/fun/epoch/core/utils/TextUtils.java
@@ -6,5 +6,11 @@ package fun.epoch.core.utils;
  * Created by epoch
  */
 public class TextUtils {
+    public static boolean isNotBlank(String str) {
+        return str != null && str.replaceAll("\\s", "").length() > 0;
+    }
 
+    public static boolean isBlank(String str) {
+        return !isNotBlank(str);
+    }
 }
